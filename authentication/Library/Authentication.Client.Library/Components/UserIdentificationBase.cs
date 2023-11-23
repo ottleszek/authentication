b43fb2ec2fce14da9948a07b6dcb625e43cd2f77
gycsaba <gycsaba72@gmail.com>
2023-11-23 22:04:14 +0100

    Profil, user debug adatok kiolvasva

---
+++ authentication/Library/Authentication.Client.Library/Components/UserIdentificationBase.cs
@@ -0,0 +1,123 @@
+﻿using Authentication.Shared.Models;
+using AuthenticationLibrary.Provider.UserIdentification;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Data;
+using System.Security.Claims;
+
+namespace Authentication.Client.Library.Components
+{
+    public class UserIdentificationBase : ComponentBase
+    {
+        [Inject] private AuthenticationStateProvider? AuthenticationStateProvider { get; set; }
+
+        protected UserIdentificationData? userIdentificationData=null;
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (AuthenticationStateProvider is not null)
+            {
+                AuthenticationState authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+                ClaimsPrincipal user = authState.User;
+                if (user.Identity is not null && user.Identity.IsAuthenticated)
+                {
+                    userIdentificationData = new();
+                    userIdentificationData.IsAuthenticated = true;
+                }
+                else
+                {
+                    userIdentificationData = new();
+                    userIdentificationData.IsAuthenticated = true;
+                }
+            }
+            await base.OnInitializedAsync();
+        }
+
+        protected async Task<string> GetUserEmail()
+        {
+            List<Claim>? claims = await GetClaims();
+            if (claims is not null)
+            {
+                string? email = claims.Where(claim => claim.Type == "Email").Select(claim => claim.Value).FirstOrDefault();
+                if (!string.IsNullOrEmpty(email))
+                {
+                    return email;
+                }
+            }
+            return string.Empty;
+        }
+
+        protected async Task<string> GetUserDisplayName()
+        {
+            List<Claim>? claims = await GetClaims();
+            if (claims is not null)
+            {
+                string? firstName = claims.Where(claim => claim.Type == "FirstName").Select(claim => claim.Value).FirstOrDefault();
+                string? lastName = claims.Where(claim => claim.Type == "LastName").Select(claim => claim.Value).FirstOrDefault();
+                if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+                {
+                    string? email = claims.Where(claim => claim.Type == "Email").Select(claim => claim.Value).FirstOrDefault();
+                    if (string.IsNullOrEmpty(email))
+                    {
+                        return string.Empty;
+                    }
+                    return email;
+                }
+                return $"{lastName} {firstName}";
+            }
+            return string.Empty;
+        }
+
+        protected async Task<string> GetUserRole()
+        {
+            List<Claim>? claims = await GetClaims();
+            if (claims is not null)
+            {
+                string? role = claims.Where(claim => claim.Type == "Szerep").Select(claim => claim.Value).FirstOrDefault();
+                if (string.IsNullOrEmpty(role))
+                {
+                    return string.Empty;
+                }
+                return role;
+            }
+            return string.Empty;
+        }
+
+        protected async Task<string> GetUserDebugData()
+        {
+            List<Claim>? claims = await GetClaims();
+            if (claims is not null)
+            {
+                string debugData = string.Empty;
+                debugData = "<table>";
+                foreach (Claim claim in claims)
+                {
+                    debugData = $"{debugData}<tr>";
+                    if (debugData.Any())
+                        debugData = $"{debugData}; <td>({claim.ValueType}</td> <td> {claim.Issuer}</td><td> {claim.Value}</td>";
+                    else
+                        debugData = $"<td>({claim.ValueType} </td><td> {claim.Issuer}</td> <td> {claim.Value}</td>";
+                    debugData = $"{debugData}</tr>";
+                }
+                return debugData;
+            }
+            return string.Empty;
+        }
+
+        private async Task<List<Claim>?> GetClaims()
+        {
+            if (AuthenticationStateProvider is not null)
+            {
+                AuthenticationState authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+                ClaimsPrincipal user = authState.User;
+                if (user.Identity is not null && user.Identity.IsAuthenticated)
+                {
+                    userIdentificationData = new();
+                    userIdentificationData.IsAuthenticated = true;
+                    return user.Claims.ToList();
+                }
+            }
+            return null;
+        }
+    }
+}
