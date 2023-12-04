f5122ca842240c28daf27195eb5442f6e7a01da7
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-12-04 18:15:22 +0100

    CreateEditUser

---
+++ authentication/Authentication/Client/Pages/UserPage/CreateEditUser.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Authentication.Client.Pages.UserPage
+{
+    public partial class CreateEditUser
+    {
+        [Parameter]public Guid Id { get; set; }
+    }
+}
