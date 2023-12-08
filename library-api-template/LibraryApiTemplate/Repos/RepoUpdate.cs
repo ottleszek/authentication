ea53a2692ac8e5563dbee421f4f46a305a5e7258
gycsaba <gycsaba72@gmail.com>
2023-12-08 19:40:54 +0100

    RepoUpdate

---
+++ library-api-template/LibraryApiTemplate/Repos/RepoUpdate.cs
@@ -0,0 +1,34 @@
+﻿using LibraryCore.Model;
+using LibraryCore.Responses;
+using LibraryDataBroker;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApiTemplate.Repos
+{
+    public class RepoUpdate<TDbContext> : RepoGet<TDbContext>, IUpdateDataBroker where TDbContext : DbContext
+    {
+        private readonly IDbContextFactory<TDbContext> _dbContextFactory;
+
+        public RepoUpdate(IDbContextFactory<TDbContext> dbContextFactory) : base(dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+        public async Task<ControllerResponse>  UpdateAsync<TEntity>(TEntity entity) where TEntity : class, IDbRecord<TEntity>, new()
+        {
+            ControllerResponse response = new ControllerResponse();
+            var dbContext = _dbContextFactory.CreateDbContext();
+
+            dbContext.ChangeTracker.Clear();
+            dbContext.Entry(entity).State = EntityState.Modified;
+            try
+            {
+                await dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                response.AppendNewError(e.Message);
+            }
+            return response;
+        }
+    }
+}
