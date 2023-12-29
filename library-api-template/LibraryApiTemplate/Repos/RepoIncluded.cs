d14677fd48010103320f3787ff0b3967c9f10484
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-12-29 10:42:43 +0100

    RepoIncluded

---
+++ library-api-template/LibraryApiTemplate/Repos/RepoIncluded.cs
@@ -0,0 +1,31 @@
+﻿using LibraryCore.Model;
+using LibraryDataBroker;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApiTemplate.Repos
+{
+    public abstract class RepoIncluded<TDbContext> : RepoList<TDbContext>, IIncludedDataBroker where TDbContext : DbContext
+    {
+        private readonly IDbContextFactory<TDbContext> _dbContextFactory;
+
+        public RepoIncluded(IDbContextFactory<TDbContext> dbContextFactory) : base(dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public Task<List<TEntity>> SelectAllRecordIncludedAsync<TEntity>() where TEntity : class, IDbRecord<TEntity>, new()
+        {
+            IQueryable<TEntity>? entitise = GetAllIncluded<TEntity>();
+            List<TEntity> list = new List<TEntity>();
+            if (entitise is not null)
+            {
+                list=entitise.ToList();
+            }
+            return list;
+        }
+
+        protected abstract IQueryable<TEntity>? GetAllIncluded<TEntity>() where TEntity : class, new();
+
+
+    }
+}
