08e9748a139edd94d5fa615affd2ed692dce8e97
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-12-29 13:17:11 +0100

    IncludedListViewModel

---
+++ library-client-service-template/LibraryClientServiceTemplate/ViewModelsTemplate/IncludedListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using LibraryClientServiceTemplate.ModelBrokerConnectors;
+using LibraryCore.Model;
+
+namespace LibraryClientServiceTemplate.ViewModelsTemplate
+{
+    public class IncludedListViewModel<TItem> : ListViewModel<TItem>, IIncludedListViewModel<TItem> where TItem : class, IDbRecord<TItem>, new()
+    {
+        private readonly IIncludedBrokerConnector<TItem> _includedConnector;
+
+        public IncludedListViewModel(IIncludedBrokerConnector<TItem> includedBrokerConnector, IListBrokerConnector<TItem> service) : base(service)
+        {
+            _includedConnector = includedBrokerConnector;
+        }
+
+        public async Task GetAllDataIncludedToViewModelAsync()
+        {
+            Items = await _includedConnector.SelectAllRecordIncludedToListAsync();
+        }
+
+        public async Task<List<TItem>> ReloadIncludedDataAsync()
+        {
+            return await _includedConnector.SelectAllRecordIncludedToListAsync();
+        }
+    }
+}
