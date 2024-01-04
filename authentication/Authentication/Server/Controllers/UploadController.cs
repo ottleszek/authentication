5f6fd61618dbeac78aa61f144ace9cd181b416c2
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-04 17:58:30 +0100

    UploadController file név variációk

--- authentication/Authentication/Server/Controllers/UploadController.cs
+++ authentication/Authentication/Server/Controllers/UploadController.cs
@@ -13,6 +13,8 @@ namespace Authentication.Server.Controllers
         public async Task<IActionResult> Upload([FromForm] UploadFileDataDto dto)
         {
             string? fileName = string.Empty;
+            if (!dto.IsValidData)
+                return BadRequest();
             try
             {
                 var formCollection = await Request.ReadFormAsync();
@@ -21,7 +23,38 @@ namespace Authentication.Server.Controllers
                 var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
                 if (file.Length > 0 && file.ContentDisposition is not null)
                 {
-                    fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName?.Trim('"');
+                    if (dto.IsNewFileName && dto.IsNewExtension)
+                    {
+                        fileName = dto.NewFileName;
+                    }
+                    else if (!dto.IsNewFileName && !dto.IsNewExtension)
+                    {
+                        fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName?.Trim('"');
+                    }
+                    else if (dto.IsNewFileName && !dto.IsNewExtension)
+                    {
+                        string? tempFileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName?.Trim('"');
+                        if (tempFileName is not null)
+                        {
+                            fileName = $"{dto.Data.FileName}.{Path.GetExtension(tempFileName)}";
+                        }
+                        else
+                        {
+                            fileName = $"{dto.Data.FileName}";
+                        }
+                    }
+                    else 
+                    {
+                        string? tempFileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName?.Trim('"');
+                        if (tempFileName is not null)
+                        {
+                            fileName= $"{Path.GetFileNameWithoutExtension(tempFileName)}.{dto.Data.FileName}";
+                        }
+                        else
+                        {
+                            fileName = $"{Guid.NewGuid().ToString()}.{dto.Data.FileName}";
+                        }
+                    }
                     if (fileName is not null)
                     {
                         var fullPath = Path.Combine(pathToSave, fileName);
--- authentication/Authentication/Server/Datas/UploadFileDataDto.cs
+++ authentication/Authentication/Server/Datas/UploadFileDataDto.cs
@@ -4,11 +4,18 @@
     {
         public IFormFile? FileToUpload { get; set; }
         public FileUploadDataDto Data { get; set; } = new();
+        public bool IsValidData => (Data.FilePath != string.Empty && Data.FileName != string.Empty && Data.Extension != string.Empty)
+                                    ||
+                                   (Data.Extension == string.Empty && Path.GetExtension(Data.FileName) != string.Empty && Path.GetFileNameWithoutExtension(Data.FileName) != string.Empty);
+        public string NewFileName => $"{Data.FileName}.{Data.Extension}";
+        public bool IsNewExtension => Data.Extension != string.Empty;
+        public bool IsNewFileName => Data.FilePath != string.Empty;
     }
 
     public class FileUploadDataDto
     {
         public string FilePath { get; set; } = string.Empty;
         public string FileName { get; set; } = string.Empty;
+        public string Extension { get; set; } = string.Empty;
     }
 }
