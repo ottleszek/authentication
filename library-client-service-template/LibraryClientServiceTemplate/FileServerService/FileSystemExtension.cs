521f3b83137027738dd5480f4b6396b809e8db95
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-10 12:52:52 +0100

    IsUrlExist

---
+++ library-client-service-template/LibraryClientServiceTemplate/FileServerService/FileSystemExtension.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+
+namespace LibraryClientServiceTemplate.FileServerService
+{
+    public class FileSystemExtension
+    {
+        public bool IsUrlExist(string url, int timeOutMs = 1000)
+        {
+            bool exists = false;
+            HttpWebResponse response = null;
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "HEAD";
+            request.Timeout = timeOutMs; // milliseconds
+            request.AllowAutoRedirect = false;
+
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+                exists = response.StatusCode == HttpStatusCode.OK;
+            }
+            catch
+            {
+                exists = false;
+            }
+            finally
+            {
+                // close your response.
+                if (response != null)
+                    response.Close();
+            }
+            return exists;
+        }
+
+        /*private readonly HttpClient? _httpClient
+        public FileSystemExtension(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("AuthenticationApi");
+        }
+
+        private async Task<bool> isFileExist(string url)
+        {
+            if (_httpClient is null)
+                return false;
+            else
+            {
+                var restponse = await _httpClient.GetAsync(url);
+                return restponse.StatusCode == System.Net.HttpStatusCode.OK;
+            }            
+        }*/
+    }
+}
