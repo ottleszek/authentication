9f525e6b82f4f73506f0ec8f6824e12cf3995ede
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-19 10:12:30 +0100

    contextOptions

---
+++ authentication/Test/Authentication.Server.Test/Repos/ProfilRepoTest.cs
@@ -0,0 +1,12 @@
+﻿using Authentication.Server.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.Server.Test.Repos
+{
+    public class ProfilRepoTest
+    {
+        public static DbContextOptions<AuthenticationContext> contextOptions = new DbContextOptionsBuilder<AuthenticationContext>()
+                .UseInMemoryDatabase(databaseName: "AuthenticationTest" + Guid.NewGuid().ToString())
+                .Options;
+    }
+}
---
+++ authentication/Test/Authentication.Server.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Authentication.Server.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
