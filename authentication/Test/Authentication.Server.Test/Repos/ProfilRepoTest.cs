47b1498c680c522f644a99d813526c21e703292b
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-19 10:21:17 +0100

    authenticationContext

--- authentication/Test/Authentication.Server.Test/Repos/ProfilRepoTest.cs
+++ authentication/Test/Authentication.Server.Test/Repos/ProfilRepoTest.cs
@@ -3,10 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Authentication.Server.Test.Repos
 {
-    public class ProfilRepoTest
+    public class ProfilRepoTest : IDisposable
     {
         public static DbContextOptions<AuthenticationContext> contextOptions = new DbContextOptionsBuilder<AuthenticationContext>()
                 .UseInMemoryDatabase(databaseName: "AuthenticationTest" + Guid.NewGuid().ToString())
                 .Options;
+
+        private AuthenticationContext authenticationContext = new AuthenticationContext(contextOptions);
+
+        public void Dispose()
+        {
+            authenticationContext.Dispose();
+        }
     }
 }
