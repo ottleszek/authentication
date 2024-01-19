7dfd7354c0675cbc167e3b3b0e0c440662201fe4
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-19 13:40:35 +0100

    Fejlesztés kell még

--- authentication/Test/Authentication.Server.Test/Repos/ProfilRepoTest.cs
+++ authentication/Test/Authentication.Server.Test/Repos/ProfilRepoTest.cs
@@ -1,19 +1,52 @@
 ﻿using Authentication.Server.Context;
+using Authentication.Server.Repos;
+using Authentication.Server.Repos.DataBroker;
+using Authentication.Shared.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Authentication.Server.Test.Repos
 {
     public class ProfilRepoTest : IDisposable
     {
-        public static DbContextOptions<AuthenticationContext> contextOptions = new DbContextOptionsBuilder<AuthenticationContext>()
+        // https://umplify.github.io/xunit-dependency-injection/
+        // https://github.com/Umplify/xunit-dependency-injection/tree/main/examples/Xunit.Microsoft.DependencyInjection.ExampleTests/Fixtures
+
+        /*public static DbContextOptions<AuthenticationContext> contextOptions = new DbContextOptionsBuilder<AuthenticationContext>()
                 .UseInMemoryDatabase(databaseName: "AuthenticationTest" + Guid.NewGuid().ToString())
                 .Options;
 
-        private AuthenticationInMemoryContext authenticationContext = new AuthenticationInMemoryContext(contextOptions);
+        private AuthenticationInMemoryContext authenticationContext = new AuthenticationInMemoryContext(contextOptions);*/
+        private IProfilRepo? _profilRepo = null;
+
+        public ProfilRepoTest(IProfilRepo? profilRepo)
+        {
+            _profilRepo = profilRepo;
+        }
+
+        public class Startup
+        {
+            public void ConfigureServices(IServiceCollection services)
+            {
+                services.AddTransient<IProfilRepo, ProfilRepoInMemoryDataBroker>();
+            }
+        }
+
 
         public void Dispose()
         {
-            authenticationContext.Dispose();
+            //authenticationContext.Dispose();
+        }
+
+        [Fact]
+        //[InlineData("admin@teszt.hu")]
+        public async Task GetByEmail()
+        {
+            if (_profilRepo is not null)
+            {
+                User? user = await _profilRepo.GetUserBy("admin@teszt.hu");
+            }
         }
     }
 }
